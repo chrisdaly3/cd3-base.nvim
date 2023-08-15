0aecbdcb8a7fd902cc40729c13787892a21178e4
Chris Daly <cdaly@urbn.com>
2023-08-15 08:37:59 -0400

    Change colorscheme. idk how I feel yet.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   -- add colorscheme
-  { "ellisonleao/gruvbox.nvim" },
+  { "danilo-augusto/vim-afterglow" },
 
   -- Now configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "afterglow",
     },
   },
 }
